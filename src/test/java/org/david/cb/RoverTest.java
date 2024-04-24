52cbbab0a5892372d02ddfd58e019d2c3123f17b
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-24 19:34:10 +0200

    Rename and split test to separate the initial coordinates and the initial orientation

--- src/test/java/org/david/cb/RoverTest.java
+++ src/test/java/org/david/cb/RoverTest.java
@@ -6,12 +6,20 @@ import org.junit.jupiter.api.Test;
 class RoverTest {
 
     @Test
-    void create_should_work() {
+    void rover_create_rover_should_be_created_in_the_indicated_coordinates() {
         Coordinates coordinates = new Coordinates(0, 0);
         Orientation orientation = Orientation.NORTH;
         Rover rover = new Rover(coordinates, orientation);
         Assertions.assertNotNull(rover);
         Assertions.assertEquals(coordinates, rover.getCoordinates());
+    }
+
+    @Test
+    void rover_create_rover_should_be_created_in_the_indicated_orientation() {
+        Coordinates coordinates = new Coordinates(0, 0);
+        Orientation orientation = Orientation.NORTH;
+        Rover rover = new Rover(coordinates, orientation);
+        Assertions.assertNotNull(rover);
         Assertions.assertEquals(orientation, rover.getOrientation());
     }
 
