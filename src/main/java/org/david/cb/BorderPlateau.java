b5c3de1c62ce727a8cda341b9ab6fccc552dc448
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-25 22:18:42 +0200

    Calculate coordinates moving to north

--- src/main/java/org/david/cb/BorderPlateau.java
+++ src/main/java/org/david/cb/BorderPlateau.java
@@ -2,8 +2,28 @@ package org.david.cb;
 
 public class BorderPlateau implements Plateau {
 
+    private final int limitX;
+    private final int limitY;
+
+    public BorderPlateau(int limitX, int limitY) {
+        this.limitX = limitX;
+        this.limitY = limitY;
+    }
+
     @Override
     public Coordinates calculatePosition(Coordinates currentCoordinates, Orientation orientation) {
+        if (Orientation.NORTH == orientation) {
+            return new Coordinates(currentCoordinates.getX(), currentCoordinates.getY() + 1);
+        }
+
         return null;
+    }
+
+    public int getLimitX() {
+        return limitX;
+    }
+
+    public int getLimitY() {
+        return limitY;
     }
 }
---
+++ src/test/java/org/david/cb/BorderPlateauTest.java
@@ -0,0 +1,39 @@
+package org.david.cb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.david.cb.Orientation.NORTH;
+
+class BorderPlateauTest {
+
+    @Test
+    void create_borderPlateau_should_determinate_the_limits() {
+        int limitX = 5;
+        int limitY = 10;
+
+        BorderPlateau borderPlateau = new BorderPlateau(limitX, limitY);
+
+        Assertions.assertEquals(limitX, borderPlateau.getLimitX());
+        Assertions.assertEquals(limitY, borderPlateau.getLimitY());
+    }
+
+    @Test
+    void calculatePosition_when_orientation_is_north_should_move_to_north() {
+
+        int limitX = 5;
+        int limitY = 10;
+        BorderPlateau borderPlateau = new BorderPlateau(limitX, limitY);
+
+        int x = 2;
+        int y = 4;
+        Coordinates coordinates = new Coordinates(x, y);
+
+        Coordinates newCoordinates = borderPlateau.calculatePosition(coordinates, NORTH);
+        Coordinates expectedCoordinates = new Coordinates(x, y + 1);
+
+        Assertions.assertEquals(expectedCoordinates, newCoordinates);
+
+    }
+
+}
