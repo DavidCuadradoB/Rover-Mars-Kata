bb64b7eb5b4f3729f237994f4c046f5685a0bf87
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-25 22:44:47 +0200

    Rename Mower

--- src/main/java/org/david/cb/Mower.java
+++ src/main/java/org/david/cb/Mower.java
@@ -1,12 +1,12 @@
 package org.david.cb;
 
-public class Rover {
+public class Mower {
 
     private final Coordinates coordinates;
     private final Orientation orientation;
     private final Plateau plateau;
 
-    public Rover(Coordinates coordinates, Orientation orientation, Plateau plateau) {
+    public Mower(Coordinates coordinates, Orientation orientation, Plateau plateau) {
         this.coordinates = coordinates;
         this.orientation = orientation;
         this.plateau = plateau;
@@ -20,55 +20,55 @@ public class Rover {
         return coordinates;
     }
 
-    public Rover execute(MoveCommand command) throws IncorrectCommandException {
+    public Mower execute(MoveCommand command) throws IncorrectCommandException {
 
-        Rover moved_rover = this;
+        Mower moved_mower = this;
 
         for (char c : command.getCommands().toCharArray()) {
             char commandUpperCase = Character.toUpperCase(c);
             if (commandUpperCase == 'L') {
-                moved_rover = rotateLeft();
+                moved_mower = rotateLeft();
             } else if (commandUpperCase == 'R') {
-                moved_rover = rotateRight();
+                moved_mower = rotateRight();
             } else if (commandUpperCase == 'M') {
-                moved_rover = moveForward();
+                moved_mower = moveForward();
             } else {
                 throw new IncorrectCommandException(c);
             }
         }
 
-        return moved_rover;
+        return moved_mower;
     }
 
-    private Rover moveForward() {
-        return new Rover(
+    private Mower moveForward() {
+        return new Mower(
                 plateau.calculatePosition(this.coordinates, this.orientation),
                 this.orientation,
                 this.plateau
         );
     }
 
-    private Rover rotateRight() {
+    private Mower rotateRight() {
         if (Orientation.NORTH == this.orientation) {
-            return new Rover(this.coordinates, Orientation.EAST, this.plateau);
+            return new Mower(this.coordinates, Orientation.EAST, this.plateau);
         } else if (Orientation.EAST == this.orientation) {
-            return new Rover(this.coordinates, Orientation.SOUTH, this.plateau);
+            return new Mower(this.coordinates, Orientation.SOUTH, this.plateau);
         } else if (Orientation.SOUTH == this.orientation) {
-            return new Rover(this.coordinates, Orientation.WEST, this.plateau);
+            return new Mower(this.coordinates, Orientation.WEST, this.plateau);
         } else {
-            return new Rover(this.coordinates, Orientation.NORTH, this.plateau);
+            return new Mower(this.coordinates, Orientation.NORTH, this.plateau);
         }
     }
 
-    private Rover rotateLeft() {
+    private Mower rotateLeft() {
         if (Orientation.NORTH == this.orientation) {
-            return new Rover(this.coordinates, Orientation.WEST, this.plateau);
+            return new Mower(this.coordinates, Orientation.WEST, this.plateau);
         } else if (Orientation.WEST == this.orientation) {
-            return new Rover(this.coordinates, Orientation.SOUTH, this.plateau);
+            return new Mower(this.coordinates, Orientation.SOUTH, this.plateau);
         } else if (Orientation.SOUTH == this.orientation) {
-            return new Rover(this.coordinates, Orientation.EAST, this.plateau);
+            return new Mower(this.coordinates, Orientation.EAST, this.plateau);
         } else {
-            return new Rover(this.coordinates, Orientation.NORTH, this.plateau);
+            return new Mower(this.coordinates, Orientation.NORTH, this.plateau);
         }
     }
 }
---
+++ src/test/java/org/david/cb/MowerTest.java
@@ -0,0 +1,167 @@
+package org.david.cb;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.david.cb.Orientation.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MowerTest {
+
+    @Mock
+    private Plateau plateau;
+
+    @Test
+    void mower_create_mower_should_be_created_in_the_indicated_coordinates() {
+        Coordinates coordinates = new Coordinates(5, 7);
+        Mower mower = new Mower(coordinates, EAST, plateau);
+        assertNotNull(mower);
+        assertEquals(coordinates, mower.getCoordinates());
+    }
+
+    @Test
+    void mower_create_mower_should_be_created_in_the_indicated_orientation() {
+        Coordinates coordinates = new Coordinates(2, 3);
+        Orientation orientation = SOUTH;
+        Mower mower = new Mower(coordinates, orientation, plateau);
+        assertNotNull(mower);
+        assertEquals(orientation, mower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"M", "m"})
+    void execute_should_calculatePosition_using_the_grid(String command) throws IncorrectCommandException {
+        Mower mower = getMower(EAST);
+        MoveCommand commands = new MoveCommand(command);
+
+        Coordinates expectedCoordinates = new Coordinates(1, 1);
+        when(plateau.calculatePosition(mower.getCoordinates(), mower.getOrientation())).thenReturn(expectedCoordinates);
+
+        Mower movedMower = mower.execute(commands);
+
+        assertEquals(movedMower.getCoordinates(), expectedCoordinates);
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "r"})
+    void execute_with_right_when_the_mower_is_facing_north_the_mower_should_rotate_to_east(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(NORTH);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(EAST, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "r"})
+    void execute_with_right_when_the_mower_is_facing_east_the_mower_should_rotate_to_south(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(EAST);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(SOUTH, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "r"})
+    void execute_with_right_when_the_mower_is_facing_south_the_mower_should_rotate_to_west(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(SOUTH);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(WEST, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "r"})
+    void execute_with_right_when_the_mower_is_facing_west_the_mower_should_rotate_to_north(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(WEST);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(NORTH, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "l"})
+    void execute_with_left_when_the_mower_is_facing_north_the_mower_should_rotate_to_west(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(NORTH);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(WEST, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "l"})
+    void execute_with_left_when_the_mower_is_facing_west_the_mower_should_rotate_to_south(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(WEST);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(SOUTH, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "l"})
+    void execute_with_left_when_the_mower_is_facing_south_the_mower_should_rotate_to_east(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(SOUTH);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(EAST, movedMower.getOrientation());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "l"})
+    void execute_with_left_when_the_mower_is_facing_east_the_mower_should_rotate_to_north(String commands)
+            throws IncorrectCommandException {
+        Mower mower = getMower(EAST);
+        MoveCommand command = new MoveCommand(commands);
+
+        Mower movedMower = mower.execute(command);
+
+        assertEquals(NORTH, movedMower.getOrientation());
+    }
+
+    @Test
+    void execute_with_incorrect_value_should_throw_an_exception() {
+        Mower mower = getMower(EAST);
+        String commands = "/";
+        MoveCommand command = new MoveCommand(commands);
+
+        IncorrectCommandException exception = assertThrows(
+                IncorrectCommandException.class, () -> mower.execute(command)
+        );
+
+        String expectedMessage = "The command : " + commands + " is not a valid character.";
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    private Mower getMower(Orientation orientation) {
+        Coordinates coordinates = new Coordinates(5, 7);
+        return new Mower(coordinates, orientation, plateau);
+    }
+}
