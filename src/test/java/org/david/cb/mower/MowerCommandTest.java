4d4f03416dc4e5ab6ee5ff60974c1b31abff2cbc
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-27 18:16:26 +0200

    Create test for MowerCommand

---
+++ src/test/java/org/david/cb/mower/MowerCommandTest.java
@@ -0,0 +1,44 @@
+package org.david.cb.mower;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MowerCommandTest {
+
+    @Test
+    void formChar_should_return_ROTATE_RIGHT_when_char_is_R() {
+
+        Optional<MowerCommand> result = MowerCommand.fromChar('R');
+
+        assertEquals(Optional.of(MowerCommand.ROTATE_RIGHT), result);
+    }
+
+    @Test
+    void formChar_should_return_ROTATE_LEFT_when_char_is_L() {
+
+        Optional<MowerCommand> result = MowerCommand.fromChar('L');
+
+        assertEquals(Optional.of(MowerCommand.ROTATE_LEFT), result);
+    }
+
+    @Test
+    void formChar_should_return_MOVE_FORWARD_when_char_is_M() {
+
+        Optional<MowerCommand> result = MowerCommand.fromChar('M');
+
+        assertEquals(Optional.of(MowerCommand.MOVE_FORWARD), result);
+    }
+
+    @Test
+    void formChar_should_return_empty_when_char_is_an_invalid_command() {
+
+        Optional<MowerCommand> result = MowerCommand.fromChar('/');
+
+        assertEquals(Optional.empty(), result);
+    }
+
+
+}
