50b3b8fa3890157900537480178ff99a30985d25
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-27 18:36:23 +0200

    Create forAbbreviation in MowerCommand

--- src/main/java/org/david/cb/mower/Orientation.java
+++ src/main/java/org/david/cb/mower/Orientation.java
@@ -1,13 +1,28 @@
 package org.david.cb.mower;
 
+import java.util.Optional;
+
 public enum Orientation {
 
-    NORTH("N"), EAST("E"), SOUTH("S"), WEST("W");
+    NORTH("N"),
+    EAST("E"),
+    SOUTH("S"),
+    WEST("W");
 
     public final String abbreviation;
 
     Orientation(String abbreviation) {
         this.abbreviation = abbreviation;
+    }
+
+    public static Optional<Orientation> forAbbreviation(String abbreviation) {
+        for (Orientation orientation : Orientation.values()) {
+            if (orientation.abbreviation.equals(abbreviation)) {
+                return Optional.of(orientation);
+            }
+        }
+        return Optional.empty();
+
     }
 
 }
---
+++ src/test/java/org/david/cb/mower/OrientationTest.java
@@ -0,0 +1,44 @@
+package org.david.cb.mower;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+class OrientationTest {
+
+    @Test
+    void formChar_should_return_NORTH_when_abbreviation_is_N() {
+        Optional<Orientation> orientation = Orientation.forAbbreviation("N");
+
+        Assertions.assertEquals(Optional.of(Orientation.NORTH), orientation);
+    }
+
+    @Test
+    void formChar_should_return_EAST_when_abbreviation_is_E() {
+        Optional<Orientation> orientation = Orientation.forAbbreviation("E");
+
+        Assertions.assertEquals(Optional.of(Orientation.EAST), orientation);
+    }
+
+    @Test
+    void formChar_should_return_SOUTH_when_abbreviation_is_S() {
+        Optional<Orientation> orientation = Orientation.forAbbreviation("S");
+
+        Assertions.assertEquals(Optional.of(Orientation.SOUTH), orientation);
+    }
+
+    @Test
+    void formChar_should_return_WEST_when_abbreviation_is_W() {
+        Optional<Orientation> orientation = Orientation.forAbbreviation("W");
+
+        Assertions.assertEquals(Optional.of(Orientation.WEST), orientation);
+    }
+
+    @Test
+    void formChar_should_return_empty_when_abbreviation_is_invalid() {
+        Optional<Orientation> orientation = Orientation.forAbbreviation("N");
+
+        Assertions.assertEquals(Optional.of(Orientation.NORTH), orientation);
+    }
+}
