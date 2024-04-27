246eb2c388281565c84b21486ae8843d4782a725
David Cuadrado <davidcuadradoballesteros@gmail.com>
2024-04-27 17:31:49 +0200

    Import spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,23 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.david.cb"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.11.0")
